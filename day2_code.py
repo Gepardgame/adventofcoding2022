a5ece49e8fb38cc1a9a65eaa1ba0d0ca004c5bef
Gepardgame <thomas.degg@gmail.com>
2022-12-03 15:11:17 +0100

    Part Two completed

--- day2_code.py
+++ day2_code.py
@@ -1,4 +1,7 @@
-f, points = [t.split(" ") for t in open("day2_input.txt", "r").read().split("\n")], 0
+f, points, points2 = [t.split(" ") for t in open("day2_input.txt", "r").read().split("\n")], 0, 0
+reverse_win = {"A": "Y", "B": "Z", "C": "X"}
+reverse_draw = {"A": "X", "B": "Y", "C": "Z"}
+reverse_loose = {"A": "Z", "B": "X", "C": "Y"}
 point = {"X": ("C", "A", 1), "Y": ("A", "B", 2), "Z": ("B", "C", 3)}
 for i, j in f:
     if point[j][0] == i:
@@ -7,4 +10,14 @@ for i, j in f:
         points += 3 + point[j][2]
     else:
         points += point[j][2]
+
+for i, j in f:
+    if j == "X":
+        points2 += point[reverse_loose[i]][2]
+    if j == "Y":
+        points2 += 3 + point[reverse_draw[i]][2]
+    if j == "Z":
+        points2 += 6 + point[reverse_win[i]][2]
+
 print(points)
+print(points2)

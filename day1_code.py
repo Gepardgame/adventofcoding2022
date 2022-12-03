0035d4f91b0d07716031fe9311ed386125b9ea6a
Gepardgame <thomas.degg@gmail.com>
2022-12-03 13:38:53 +0100

    Part Two completed

--- day1_code.py
+++ day1_code.py
@@ -1,3 +1,5 @@
+import heapq
 f = open("day1_input.txt", "r").read().split("\n\n")
 calories = [sum([int(i) for i in calorie1.split("\n") if type(i) == int or i.isdigit()]) for calorie1 in f]
 print(max(calories))
+print(sum(heapq.nlargest(3, calories)))
